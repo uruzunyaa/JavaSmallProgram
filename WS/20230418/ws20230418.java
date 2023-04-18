fe4f3a3c035563cd3e178e0f63d4503aee45d4a2
uruzunyaa <110530704+uruzunyaa@users.noreply.github.com>
2023-04-18 19:31:32 +0900

    WS4月18日分の追加

--- WS/20230418/ws20230418.java
+++ WS/20230418/ws20230418.java
@@ -1,22 +1,28 @@
-import java.time.DayOfWeek;
+//入力された日付の曜日を出力するプログラム
+//入力方法はスペース区切り。"2020 3 5"　のように、コマンドライン引数に入力する。
+
+//日付管理系のクラス等をインポート
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 public class ws20230418 {
     public static void main(String[] args) {
-        // 現在の日付を取得
-        LocalDate today = LocalDate.now();
+        
+        //曜日の略称を定義
+        String[] weekdays = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        
+        //入力値を"yyyy-MM-dd"の形に変換、これがないと一桁の数字でエラーが起きるため。
+        String formattedDateString = args[0]+"-"+String.format("%02d", Integer.parseInt(args[1]))+"-"+String.format("%02d", Integer.parseInt(args[2]));
+        
+        //日付をLocalDateクラスのインスタンスに変換
+        LocalDate date = LocalDate.parse(formattedDateString, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        
+        //曜日の値を取得　0(月曜日) 〜 6(日曜日)　の形式にする
+        int dayValue = date.getDayOfWeek().getValue()-1;
 
-        // 現在の曜日を取得
-        DayOfWeek dayOfWeek = today.getDayOfWeek();
-
-        // 曜日の値を取得
-        int dayValue = dayOfWeek.getValue();
-
-        // 曜日の文字列を取得
-        String dayString = dayOfWeek.toString();
-
-        // 結果を出力
-        System.out.println("現在の曜日の値: " + dayValue); // 1(月曜日) 〜 7(日曜日)の値を返す
-        System.out.println("現在の曜日の文字列: " + dayString); // "MONDAY"〜"SUNDAY"の文字列を返す
+        //曜日を出力
+        System.out.println(weekdays[dayValue]);
+        
     }
 }
